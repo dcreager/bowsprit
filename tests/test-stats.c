0705bfa976feeabaa41b84fd0eeb13a56ef25c96
Douglas Creager <douglas.creager@redjack.com>
2014-11-13 12:42:13 -0500

    Gauges
    This patch adds a gauge measurement class, plus all of the underlying
    contexts and plugins and whatnot.  The data model is lifted directly
    from collectd, since why not?

---
+++ tests/test-stats.c
@@ -0,0 +1,184 @@
+/* -*- coding: utf-8 -*-
+ * ----------------------------------------------------------------------
+ * Copyright © 2014, RedJack, LLC.
+ * All rights reserved.
+ *
+ * Please see the COPYING file in this distribution for license details.
+ * ----------------------------------------------------------------------
+ */
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <check.h>
+#include <libcork/core.h>
+#include <libcork/ds.h>
+
+#include "bowsprit.h"
+
+#include "helpers.h"
+
+
+/*-----------------------------------------------------------------------
+ * Statistics
+ */
+
+static void
+check_render(struct bws_ctx *ctx, const char *expected)
+{
+    struct cork_buffer  buf = CORK_BUFFER_INIT();
+    struct bws_snapshot  *snapshot = bws_snapshot_new();
+    bws_ctx_snapshot(ctx, snapshot);
+    bws_snapshot_sort(snapshot);
+    bws_snapshot_render_to_buffer(snapshot, &buf);
+    fail_unless_streq("Rendering", expected, (char *) buf.buf);
+    bws_snapshot_free(snapshot);
+    cork_buffer_done(&buf);
+}
+
+START_TEST(test_nothing)
+{
+    struct bws_ctx  *ctx;
+    struct bws_plugin  *plugin;
+
+    DESCRIBE_TEST;
+
+    ctx = bws_ctx_new("localhost");
+    plugin = bws_plugin_new(ctx, "animals", NULL);
+    bws_gauge_new(plugin, "ribbits", "frog");
+    bws_gauge_new(plugin, "meows", "cat");
+    bws_gauge_new(plugin, "barks", "dog");
+
+    check_render
+        (ctx,
+         "0 animals/barks-dog\n"
+         "0 animals/meows-cat\n"
+         "0 animals/ribbits-frog\n"
+         );
+
+    bws_ctx_free(ctx);
+}
+END_TEST
+
+START_TEST(test_gauge_01)
+{
+    struct bws_ctx  *ctx;
+    struct bws_plugin  *plugin;
+    struct bws_gauge  *frog;
+    struct bws_gauge  *cat;
+    struct bws_gauge  *dog;
+
+    DESCRIBE_TEST;
+
+    ctx = bws_ctx_new("localhost");
+    plugin = bws_plugin_new(ctx, "animals", NULL);
+    frog = bws_gauge_new(plugin, "ribbits", "frog");
+    cat = bws_gauge_new(plugin, "meows", "cat");
+    dog = bws_gauge_new(plugin, "barks", "dog");
+
+    bws_gauge_inc(frog);
+    bws_gauge_add(cat, 10);
+    bws_gauge_inc(frog);
+    bws_gauge_inc(frog);
+    bws_gauge_add(dog, 10);
+
+    check_render
+        (ctx,
+         "10 animals/barks-dog\n"
+         "10 animals/meows-cat\n"
+         " 3 animals/ribbits-frog\n"
+         );
+
+    bws_gauge_inc(frog);
+    bws_gauge_sub(cat, 5);
+
+    check_render
+        (ctx,
+         "10 animals/barks-dog\n"
+         " 5 animals/meows-cat\n"
+         " 4 animals/ribbits-frog\n"
+         );
+
+    bws_gauge_dec(frog);
+    bws_gauge_dec(frog);
+    bws_gauge_sub(dog, 10);
+
+    check_render
+        (ctx,
+         "5 animals/meows-cat\n"
+         "2 animals/ribbits-frog\n"
+         "0 animals/barks-dog\n"
+         );
+
+    bws_ctx_free(ctx);
+}
+END_TEST
+
+START_TEST(test_multi_01)
+{
+    struct bws_ctx  *ctx;
+    struct bws_plugin  *plugin;
+    struct bws_measurement  *if_packets;
+    struct bws_gauge  *tx;
+    struct bws_gauge  *rx;
+
+    DESCRIBE_TEST;
+
+    ctx = bws_ctx_new("localhost");
+    plugin = bws_plugin_new(ctx, "network", NULL);
+    if_packets = bws_measurement_new(plugin, "if_packets", "eth0");
+    /* TODO: These are really derives */
+    tx = bws_measurement_add_gauge(if_packets, "tx");
+    rx = bws_measurement_add_gauge(if_packets, "rx");
+
+    bws_gauge_add(tx, 10);
+    bws_gauge_add(rx, 20);
+
+    check_render
+        (ctx,
+         "20 network/if_packets-eth0/rx\n"
+         "10 network/if_packets-eth0/tx\n"
+         );
+
+    bws_ctx_free(ctx);
+}
+END_TEST
+
+
+/*-----------------------------------------------------------------------
+ * Testing harness
+ */
+
+Suite *
+test_suite()
+{
+    Suite  *s = suite_create("stats");
+
+    TCase  *tc_stats = tcase_create("stats");
+    tcase_add_test(tc_stats, test_nothing);
+    tcase_add_test(tc_stats, test_gauge_01);
+    tcase_add_test(tc_stats, test_multi_01);
+    suite_add_tcase(s, tc_stats);
+
+    return s;
+}
+
+
+int
+main(int argc, const char **argv)
+{
+    int  number_failed;
+    Suite  *suite = test_suite();
+    SRunner  *runner = srunner_create(suite);
+
+    initialize_tests();
+    srunner_run_all(runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    finalize_tests();
+
+    return (number_failed == 0)? EXIT_SUCCESS: EXIT_FAILURE;
+}
